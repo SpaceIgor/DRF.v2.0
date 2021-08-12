6a90b727a1faf738792cbadea08a9a5b75b7c7f6
VladPoros <pve9512@gmail.com>
2021-08-13 01:10:10 +0300

    Task15

--- tutorial/quickstart/models.py
+++ tutorial/quickstart/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    post_text = models.CharField(max_length=200)
+    opened = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.post_text
+
+
+class Comment(models.Model):
+    comment_text = models.TextField(max_length=1000)
+    comments = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return self.comment_text
+
+
+class Category(models.Model):
+    category_text = models.CharField(max_length=200)
+    category_name = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.category_text

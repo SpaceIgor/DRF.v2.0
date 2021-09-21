56a600e6d836695bb53b6f4b747460dcf25fbb48
Igor Sivak <igorsivak97@gmail.com>
2021-09-21 12:43:25 +0300

    task_17

--- tutorial/quickstart/urls.py
+++ tutorial/quickstart/urls.py
@@ -1,13 +1,12 @@
-#from django.urls import include, path
 from rest_framework import routers
-from tutorial.quickstart.views import*
+from tutorial.quickstart.views import *
+from django.views.decorators.cache import cache_page
 
 router = routers.DefaultRouter()
-router.register(r'post', PostView)
-router.register(r'comment', CommentView)
-router.register(r'category', CategoryView)
+cache_page(80)(router.register(r'post', PostView))
+cache_page(80)(router.register(r'comment', CommentView))
+cache_page(80)(router.register(r'category', CategoryView))
 
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = router.urls
+erns = router.urls
